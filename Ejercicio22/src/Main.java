a210df250c440b7dc38d8d815f167a9c55b7bdb0
jacobmv15 <jmoratovaro1@gmail.com>
2026-01-15 16:53:30 +0100

    Ejercicio 22 Uso de break y continue en bucles

---
+++ Ejercicio22/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // break = break out of a loop (STOP)
+        // continue = skip current iteration of a loop (SKIP)
+
+        for(int i = 0; i < 10; i++){
+
+            if(i == 5){
+                //break;
+                continue;
+            }
+            
+            System.out.print(i + " ");
+        }
+    }
+}
